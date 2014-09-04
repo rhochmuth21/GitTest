21282b62ae015ffa7c4857234f322f1bc979abe3
Ryan Hochmuth <rhochmuth21@gmail.com>
2014-09-04 19:09:21 -0400

    Commit GitTest.cpp, GitTest.h

---
+++ GitTest.cpp
@@ -0,0 +1,18 @@
+/*
+ * GitTest.cpp
+ *
+ *  Created on: Sep 4, 2014
+ *      Author: Handirocker21
+ */
+
+#include "GitTest.h"
+
+GitTest::GitTest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GitTest::~GitTest() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ GitTest.h
@@ -0,0 +1,17 @@
+/*
+ * GitTest.h
+ *
+ *  Created on: Sep 4, 2014
+ *      Author: Handirocker21
+ */
+
+#ifndef GITTEST_H_
+#define GITTEST_H_
+
+class GitTest {
+public:
+	GitTest();
+	virtual ~GitTest();
+};
+
+#endif /* GITTEST_H_ */
