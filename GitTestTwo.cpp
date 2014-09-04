d984ab973d346883308558fdac3444f63e1dacf5
Ryan Hochmuth <rhochmuth21@gmail.com>
2014-09-04 19:08:34 -0400

    Initial commit

---
+++ GitTestTwo.cpp
@@ -0,0 +1,18 @@
+/*
+ * GitTestTwo.cpp
+ *
+ *  Created on: Sep 4, 2014
+ *      Author: Handirocker21
+ */
+
+#include "GitTestTwo.h"
+
+GitTestTwo::GitTestTwo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GitTestTwo::~GitTestTwo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ GitTestTwo.h
@@ -0,0 +1,17 @@
+/*
+ * GitTestTwo.h
+ *
+ *  Created on: Sep 4, 2014
+ *      Author: Handirocker21
+ */
+
+#ifndef GITTESTTWO_H_
+#define GITTESTTWO_H_
+
+class GitTestTwo {
+public:
+	GitTestTwo();
+	virtual ~GitTestTwo();
+};
+
+#endif /* GITTESTTWO_H_ */
