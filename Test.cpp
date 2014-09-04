d17a223b6ea35417cd9c5d557f3818210f9db3da
Ryan Hochmuth <rhochmuth21@gmail.com>
2014-09-04 19:12:59 -0400

    Committed Test.cpp, Test.h

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: Sep 4, 2014
+ *      Author: Handirocker21
+ */
+
+#include "Test.h"
+
+Test::Test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Test::~Test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Test.h
@@ -0,0 +1,17 @@
+/*
+ * Test.h
+ *
+ *  Created on: Sep 4, 2014
+ *      Author: Handirocker21
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class Test {
+public:
+	Test();
+	virtual ~Test();
+};
+
+#endif /* TEST_H_ */
